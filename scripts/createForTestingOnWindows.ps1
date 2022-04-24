e323fa21754a646cc80ec5a1bd5044b37b556a97
David Sherret <dsherret@gmail.com>
2022-04-23 20:20:59 -0400

    0.6.4

--- scripts/createForTestingOnWindows.ps1
+++ scripts/createForTestingOnWindows.ps1
@@ -1,7 +1,7 @@
 # Script for quickly creating the plugin for testing purposes on Windows
 # To run:
 # 1. Run `./scripts/createForTestingOnWindows.ps1`
-# 2. Update dprint.json to point at ./plugin.exe-plugin then update checksum
+# 2. Update dprint.json to point at ./plugin.json then update checksum
 #    as shown when initially run.
 
 $ErrorActionPreference = "Stop"
