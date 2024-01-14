4aec1c8937ec9a0e4a846bf9a2bf23ef54b47f33
David Sherret <dsherret@gmail.com>
2024-01-13 21:12:33 -0500

    chore: generated release notes

---
+++ scripts/generate_release_notes.ts
@@ -0,0 +1,38 @@
+import { generateChangeLog } from "https://raw.githubusercontent.com/dprint/automation/0.9.0/changelog.ts";
+
+const version = Deno.args[0];
+const checksum = Deno.args[1];
+const changelog = await generateChangeLog({
+  versionTo: version,
+});
+const text = `## Changes
+
+${changelog}
+
+## Install
+
+In a dprint configuration file:
+
+1. Specify the plugin url and checksum in the \`"plugins"\` array or run \`dprint config add roslyn\`.
+   \`\`\`jsonc
+   {
+     // etc...
+     "plugins": [
+       "https://plugins.dprint.dev/roslyn-${version}.json@${checksum}"
+     ]
+   }
+   \`\`\`
+2. Add a "roslyn" configuration property if desired.
+
+   \`\`\`jsonc
+   {
+     // ...etc...
+     "roslyn": {
+       "csharp.indentBlock": false,
+       "visualBasic.indentWidth": 2
+     }
+   }
+   \`\`\`
+`;
+
+console.log(text);
