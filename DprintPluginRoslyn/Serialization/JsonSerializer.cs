58e53914d6c05151ef393bae983ee46ff665c86c
Ciaran Liedeman <ciaran@stackworx.io>
2024-02-18 13:56:32 +0200

    feat: Migrate to System.Text.Json

--- DprintPluginRoslyn/Serialization/JsonSerializer.cs
+++ DprintPluginRoslyn/Serialization/JsonSerializer.cs
@@ -1,32 +1,29 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
-using System;
-using System.Text;
+﻿using System;
 
 namespace Dprint.Plugins.Roslyn.Serialization;
+
+using System.Text;
+using System.Text.Json;
 
 public class JsonSerializer
 {
     public T Deserialize<T>(byte[] jsonData)
     {
         var jsonText = Encoding.UTF8.GetString(jsonData);
-        return JsonConvert.DeserializeObject<T>(jsonText, GetSettings()) ?? throw new Exception("Error deserializing JSON.");
+        return System.Text.Json.JsonSerializer.Deserialize<T>(jsonText, GetSettings()) ?? throw new Exception("Error deserializing JSON.");
     }
 
     public byte[] Serialize<T>(T obj)
     {
-        var jsonText = JsonConvert.SerializeObject(obj, GetSettings()) ?? throw new Exception("Error serializing to JSON.");
+        var jsonText = System.Text.Json.JsonSerializer.Serialize(obj, GetSettings()) ?? throw new Exception("Error serializing to JSON.");
         return Encoding.UTF8.GetBytes(jsonText);
     }
 
-    private JsonSerializerSettings GetSettings()
+    private JsonSerializerOptions GetSettings()
     {
-        return new JsonSerializerSettings
+        return new JsonSerializerOptions
         {
-            ContractResolver = new DefaultContractResolver
-            {
-                NamingStrategy = new CamelCaseNamingStrategy()
-            }
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
         };
     }
 }
